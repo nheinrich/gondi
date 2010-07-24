f92e32618884dd67eb61a6572e091114ecc7d13c
Neil Heinrich <neil@pixels-and-bits.com>
2010-07-23 20:04:14 -0700

    google analytics for prod

--- public/javascripts/patterns.js
+++ public/javascripts/patterns.js
@@ -66,6 +66,22 @@ var forms = {
   }
 }
 
+// google analytics ----------------------------------------------------------
+
+var gat = function(code) {
+  if (code) {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', code]);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  }
+}
+
 // hoverable -----------------------------------------------------------------
 
 var hoverable = {
