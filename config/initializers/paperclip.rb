fd32f20bd885dc98eb629f5974e73fd945d104d9
Neil Heinrich <neil@pixels-and-bits.com>
2010-08-02 14:55:17 -0700

    paperclip: fixing initializer for production

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,3 @@
-if RAILS_ENV == "development"
-  Paperclip.options[:command_path] = "/opt/local/bin"
-elsif RAILS_ENV == "production"
+if RAILS_ENV == "production"
   Paperclip.options[:command_path] = "/usr/local/bin"
 end
