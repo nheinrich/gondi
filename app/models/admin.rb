0165a6ab9a3bb604d85d401e3e563ac82c5b2afc
Neil Heinrich <neil@pixels-and-bits.com>
2010-08-02 14:42:04 -0700

    admins: remove timeout

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :trackable, :timeoutable, :validatable
+  devise :database_authenticatable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password
