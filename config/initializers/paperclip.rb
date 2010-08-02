2309d01ff1f5e620c49e77ab396f23d3ec30c082
Neil Heinrich <neil@pixels-and-bits.com>
2010-08-02 14:42:51 -0700

    paperclip: more issues saving images on the server

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,5 @@
 if RAILS_ENV == "development"
   Paperclip.options[:command_path] = "/opt/local/bin"
-end
-if RAILS_ENV == "production"
+elsif RAILS_ENV == "production"
   Paperclip.options[:command_path] = "/usr/local/bin"
 end
